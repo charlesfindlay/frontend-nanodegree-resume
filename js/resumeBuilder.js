c9c7b136bcdaa60c82d1d4b1bc0594f235c423da
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-08 16:42:22 -0400

    Used append to add fun thoughts

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,6 @@
 $("#main").append(["Charles Findlay"]);
+var awesomethoughts = "I am Charles Findlay and I am AWESOME!";
+console.log(awesomethoughts);
+var funthoughts = awesomethoughts.replace("AWESOME", "FUN");
+console.log(funthoughts);
+$("#main").append(funthoughts);
