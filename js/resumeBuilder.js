ece669435e8ce1208212a0c9ab201e712cdfedcf
Charles Findlay <charlesfindlay2000-github@yahoo.com>
2015-04-16 22:11:38 -0400

    Added formatted name and role
    Commented out the fun, learning code the lesson started with. Then
    formatted my name and desired role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,14 @@
+/*
 $("#main").append(["Charles Findlay"]);
 var awesomethoughts = "I am Charles Findlay and I am AWESOME!";
 console.log(awesomethoughts);
 var funthoughts = awesomethoughts.replace("AWESOME", "FUN");
 console.log(funthoughts);
 $("#main").append(funthoughts);
+*/
+
+// Start of real formatting code
+var formattedName = HTMLheaderName.replace("%data%", "Charles Findlay");
+var formattedRole = HTMLheaderRole.replace("%data%", ".NET Developer");
+$("#header").prepend(formattedName);
+$("#header").append(formattedRole);
